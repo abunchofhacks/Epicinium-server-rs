99a9d2a27a21f4e216ca9f26151a1c630e7bcea6
Sander in 't Veld <sanderintveld@hotmail.com>
2018-08-15 12:02:55 +0200

    Tiny bit of work on automaton.

--- src/logic/automaton.rs
+++ src/logic/automaton.rs
@@ -40,9 +40,42 @@ pub struct Automaton
 	activesubjects : HashMap<u32, Descriptor>,
 	unfinishedsubjects : HashMap<u32, Descriptor>,
 	changesets : VecDeque<ChangeSet>
+
+	// TODO recording
+	// TODO replay
+	// TODO challenge
 }
 
 impl Automaton
 {
+	pub fn initialize(&mut self, players : Vec<Player>, bible : Bible)
+	{
+		self.players = players;
+		// TODO erase NONE
 
+		self.bible = bible;
+
+		self.gameover = false;
+		self.round = 0;
+		self.year = 1;
+		self.season = Season::SPRING;
+		self.daytime = Daytime::LATE;
+		self.phase = Phase::GROWTH;
+	}
+
+	// TODO param metadata
+	// TODO param enableRecording
+	pub fn load(&mut self, map : & Map, shufflePlayers : bool)
+	{
+		self.board.load(map);
+
+		if (shufflePlayers)
+		{
+			// TODO shuffle players
+			self.board.assignPlayers(self.players);
+		}
+		else self.board.setPlayers(self.players);
+
+		// TODO finish
+	}
 }
