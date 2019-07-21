6277385a282eb96c63e4bd87e0cbb975ad1bf7cc
Sander in 't Veld <sanderintveld@hotmail.com>
2019-07-21 16:36:30 +0200

    Got my sublime text RustFmt plugin to call 'rustfmt +nightly'.

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -4,12 +4,18 @@ extern crate epicinium;
 
 use epicinium::*;
 
-
 fn main()
 {
 	let logname = String::from("rust");
 	let currentversion = Version::current();
-	println!("[ Epicinium Server ] ({} v{})",
+
+	println!(
+		"[ Epicinium Server ] ({} v{})",
 		logname,
-		currentversion.to_string());
+		currentversion.to_string()
+	);
+	println!("");
+
+	println!("");
+	println!("[ Done ]");
 }
