a704b31990654dd783e242db55aceb651a0d1083
Sander in 't Veld <sanderintveld@hotmail.com>
2019-08-17 20:40:09 +0200

    Starting over again again using tokio_io::io::read_exact() and futures::stream::unfold(), but stumped by error. (#1)

--- src/server/tokioserver.rs
+++ src/server/tokioserver.rs
@@ -60,223 +60,50 @@ pub fn run_server(settings: &Settings) -> Result<(), Box<dyn error::Error>>
 	Ok(())
 }
 
-struct ClientConnection
-{
-	stream: TcpStream,
-	active_receive_length: Option<u32>,
-	chunk_incoming: bool,
-
-	versioned: bool,
-
-	last_receive_time: time::Instant,
-	last_queue_time: time::Instant,
-	ping_send_time: Option<time::Instant>,
-	last_known_ping: time::Duration,
-	ping_tolerance: time::Duration,
-}
-
-impl ClientConnection
-{
-	fn new(stream: TcpStream) -> Self
-	{
-		ClientConnection {
-			stream: stream,
-			active_receive_length: None,
-			chunk_incoming: false,
-
-			versioned: false,
-
-			last_receive_time: time::Instant::now(),
-			last_queue_time: time::Instant::now(),
-			ping_send_time: None,
-			last_known_ping: time::Duration::from_secs(0),
-			// The client should reset the connection after 71 seconds of
-			// no contact with the server. Therefore, a 2-minute tolerance
-			// seems reasonable.
-			ping_tolerance: time::Duration::from_secs(120),
-		}
-	}
-
-	fn receive(&mut self) -> io::Result<Message>
-	{
-		let length: u32;
-		match self.active_receive_length
-		{
-			Some(len) =>
-			{
-				length = len;
-			}
-			None =>
-			{
-				let mut lengthbuffer = [0u8; 4];
-				self.stream.read_exact(&mut lengthbuffer)?;
-
-				length = u32::from_le_bytes(lengthbuffer);
-				self.active_receive_length = Some(length);
-			}
-		}
-
-		if length == 0
-		{
-			println!("Received pulse.");
-			self.active_receive_length = None;
-
-			// An empty message (i.e. without a body) is a pulse message.
-			// We just received something, thus the client is not silent.
-			self.last_receive_time = time::Instant::now();
-
-			return Ok(Message::Pulse);
-		}
-		else if self.versioned
-			&& length as usize >= MESSAGE_SIZE_UNVERSIONED_LIMIT
-		{
-			println!(
-				"Unversioned client tried to send very large message of length \
-				 {}, which is more than MESSAGE_SIZE_UNVERSIONED_LIMIT",
-				length
-			);
-			return Err(io::Error::new(
-				io::ErrorKind::InvalidInput,
-				"Message too large".to_string(),
-			));
-		}
-		else if length as usize >= MESSAGE_SIZE_LIMIT
-		{
-			println!(
-				"Refusing to receive very large message of length \
-				 {}, which is more than MESSAGE_SIZE_LIMIT",
-				length
-			);
-			return Err(io::Error::new(
-				io::ErrorKind::InvalidInput,
-				"Message too large".to_string(),
-			));
-		}
-		else if length as usize >= MESSAGE_SIZE_WARNING_LIMIT
-		{
-			println!("Receiving very large message of length {}", length);
-		}
-
-		println!("Receiving message of length {}...", length);
-
-		let mut buffer = vec![0; length as usize];
-		self.stream.read_exact(&mut buffer)?;
-		self.active_receive_length = None;
-
-		println!("Received message of length {}.", length);
-
-		// We just received something, thus the client is not silent.
-		self.last_receive_time = time::Instant::now();
-
-		// TODO if download
-		if self.chunk_incoming
-		{
-			panic!("Not implemented yet.");
-		}
-		else if buffer[0] == '=' as u8
-		{
-			panic!("Not implemented yet.");
-		}
-		else
-		{
-			let jsonstr = match String::from_utf8(buffer)
-			{
-				Ok(x) => x,
-				Err(e) =>
-				{
-					return Err(io::Error::new(io::ErrorKind::InvalidData, e));
-				}
-			};
-
-			if jsonstr.len() < 200
-			{
-				println!("Received message: {}", jsonstr);
-			}
-
-			let message: Message = serde_json::from_str(&jsonstr)?;
-
-			Ok(message)
-		}
-	}
-}
-
-impl Stream for ClientConnection
-{
-	type Item = Message;
-	type Error = io::Error;
-
-	fn poll(&mut self) -> Result<Async<Option<Self::Item>>, Self::Error>
-	{
-		match self.receive()
-		{
-			Ok(message) => Ok(Async::Ready(Some(message))),
-			Err(ref e) if e.kind() == io::ErrorKind::WouldBlock =>
-			{
-				Ok(Async::NotReady)
-			}
-			Err(ref e) if e.kind() == io::ErrorKind::UnexpectedEof =>
-			{
-				// The client has disconnected.
-				println!("Client ungracefully disconnected.");
-				Ok(Async::Ready(None))
-			}
-			Err(e) => Err(e),
-		}
-	}
-}
-
-struct Client
-{
-	connection: ClientConnection,
-
-	pub id: String,
-	pub version: Version,
-}
-
-impl Client
-{
-	fn new(stream: TcpStream) -> io::Result<Self>
-	{
-		let id = format!("{}", stream.peer_addr()?);
-
-		Ok(Client {
-			connection: ClientConnection::new(stream),
-
-			id: id,
-			version: Version::undefined(),
-		})
-	}
-}
-
-impl Future for Client
-{
-	type Item = ();
-	type Error = io::Error;
-
-	fn poll(&mut self) -> Poll<(), io::Error>
-	{
-		while let Async::Ready(activity) = self.connection.poll()?
-		{
-			if let Some(message) = activity
-			{
-				// TODO handle
-				println!("Message: {:?}", message);
-			}
-			else
-			{
-				// No more incoming messages.
-				return Ok(Async::Ready(()));
-			}
-		}
-
-		Ok(Async::NotReady)
-	}
-}
-
 fn accept_client(socket: TcpStream) -> io::Result<()>
 {
-	let client = Client::new(socket)?;
-	let task = client.map_err(move |e| eprintln!("Error in client: {}", e));
+	let task = futures::stream::unfold(socket, move |socket| {
+		let lengthbuffer = [0u8; 4];
+		let future_length = tokio_io::io::read_exact(socket, lengthbuffer)
+			.and_then(|(socket, lengthbuffer)| {
+				let length = u32::from_le_bytes(lengthbuffer);
+				let buffer = vec![0; length as usize];
+				let future_data = tokio_io::io::read_exact(socket, buffer)
+					.and_then(|(socket, buffer)| {
+						let jsonstr = match String::from_utf8(buffer)
+						{
+							Ok(x) => x,
+							Err(e) =>
+							{
+								return Err(io::Error::new(
+									io::ErrorKind::InvalidData,
+									e,
+								));
+							}
+						};
+
+						if jsonstr.len() < 200
+						{
+							println!("Received message: {}", jsonstr);
+						}
+
+						let message: Message = serde_json::from_str(&jsonstr)?;
+
+						// Unfold excepts the value first and the state second.
+						Ok((message, socket))
+					});
+
+				Ok(future_data)
+			})
+			.flatten();
+
+		Some(future_length)
+	})
+	.for_each(|message: Message| {
+		// TODO handle
+		println!("Message: {:?}", message);
+	})
+	.map_err(move |e| eprintln!("Error in client: {}", e));
 
 	tokio::spawn(task);
 
