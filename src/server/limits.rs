c04f464fe03d4e8737cead078c33147d1722c757
Sander in 't Veld <sanderintveld@hotmail.com>
2019-09-06 17:39:59 +0200

    Added underscores to VIRTUAL_SIZE_LIMITs to silence some warnings.

--- src/server/limits.rs
+++ src/server/limits.rs
@@ -5,5 +5,5 @@ pub const MESSAGE_SIZE_LIMIT: usize = 524288;
 pub const MESSAGE_SIZE_UNVERSIONED_LIMIT: usize = 201;
 pub const MESSAGE_SIZE_WARNING_LIMIT: usize = 65537;
 pub const SEND_FILE_CHUNK_SIZE: usize = 65536;
-pub const SEND_VIRTUAL_SIZE_LIMIT: usize = 65536;
-pub const SEND_VIRTUAL_SIZE_WARNING_LIMIT: usize = 8193;
+pub const _SEND_VIRTUAL_SIZE_LIMIT: usize = 65536;
+pub const _SEND_VIRTUAL_SIZE_WARNING_LIMIT: usize = 8193;
