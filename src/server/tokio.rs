771ad733b1d077a8414059255e498ae6c1e62ffa
Sander in 't Veld <sanderintveld@hotmail.com>
2019-09-07 19:48:27 +0200

    Struggling to find a good way to close the server when all clients have disconnected. (#1)

--- src/server/tokio.rs
+++ src/server/tokio.rs
@@ -32,8 +32,12 @@ pub fn run_server(settings: &Settings) -> Result<(), Box<dyn error::Error>>
 	let pkey: PrivateKey = openssl::pkey::PKey::private_key_from_pem(&pem)?;
 	let privatekey = sync::Arc::new(pkey);
 
-	let (killcount_in, killcount_out) = watch::channel(0u8);
+	let (killcount_in, killcount) = watch::channel(0u8);
 	let killer_task = killer::start_task(killcount_in);
+
+	let (keepalive, keepalive_out) = mpsc::channel::<()>(0);
+	let close_task = start_close_task(keepalive.clone(), killcount.clone());
+	let keepalive_task = start_keepalive_task(keepalive_out);
 
 	let login_server = login::connect(settings)?;
 	let login = sync::Arc::new(login_server);
@@ -42,16 +46,15 @@ pub fn run_server(settings: &Settings) -> Result<(), Box<dyn error::Error>>
 	let chat_task = chat::start_task(general_out);
 
 	let client_task = start_acceptance_task(
-		listener,
-		login,
-		general_in,
-		killcount_out,
-		privatekey,
+		listener, login, general_in, killcount, keepalive, privatekey,
 	);
 
 	let server = client_task
-		.join3(chat_task, killer_task)
-		.map(|((), (), ())| ());
+		.join4(chat_task, killer_task, close_task)
+		.map(|((), (), (), ())| ())
+		.select(keepalive_task)
+		.map(|((), _other_future)| ())
+		.map_err(|(error, _other_future)| error);
 
 	tokio::run(server);
 
@@ -63,6 +66,7 @@ fn start_acceptance_task(
 	login: sync::Arc<login::Server>,
 	chat: mpsc::Sender<chat::Update>,
 	killcount: watch::Receiver<u8>,
+	keepalive: mpsc::Sender<()>,
 	privatekey: sync::Arc<PrivateKey>,
 ) -> impl Future<Item = (), Error = ()> + Send
 {
@@ -84,6 +88,7 @@ fn start_acceptance_task(
 				login.clone(),
 				chat.clone(),
 				killcount.clone(),
+				keepalive.clone(),
 				privatekey.clone(),
 			)
 			.map(|()| println!("Accepted client {}.", id))
@@ -91,4 +96,30 @@ fn start_acceptance_task(
 		.map_err(|e| {
 			eprintln!("Incoming connection failed: {:?}", e);
 		})
+}
+
+fn start_close_task(
+	keepalive: mpsc::Sender<()>,
+	killcount: watch::Receiver<u8>,
+) -> impl Future<Item = (), Error = ()> + Send
+{
+	killcount
+		.skip(1)
+		.into_future()
+		.map_err(|(error, _)| eprintln!("Error in finish task: {:?}", error))
+		.and_then(move |(_, _)| {
+			// Drop the keepalive once this future is done.
+			let _dropped = keepalive;
+			Ok(())
+		})
+}
+
+fn start_keepalive_task(
+	keepalive: mpsc::Receiver<()>,
+) -> impl Future<Item = (), Error = ()> + Send
+{
+	keepalive
+		.for_each(|()| Ok(()))
+		.map(|()| debug_assert!(false, "Keepalive should have 0 capacity"))
+		.or_else(|_error| Ok(()))
 }
