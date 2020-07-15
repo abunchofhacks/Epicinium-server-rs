a33f1b1b3d583c52120add9467982c7bf34bb4d9
Sander in 't Veld <sanderintveld@hotmail.com>
2020-07-15 15:28:48 +0200

    Improved fern usage so that it detects SIGHUP sent by logrotate, but maybe slog is better. (#3)

--- src/common/log.rs
+++ src/common/log.rs
@@ -2,11 +2,18 @@
 
 pub fn start(logname: &str) -> Result<(), fern::InitError>
 {
-	let tracelogfilename =
-		format_args!("logs/{}.trace.log", logname).to_string();
-	let infologfilename = format_args!("logs/{}.info.log", logname).to_string();
-	let errorlogfilename =
-		format_args!("logs/{}.error.log", logname).to_string();
+	let tracelogfilename: std::path::PathBuf =
+		format_args!("logs/{}.trace.log", logname)
+			.to_string()
+			.into();
+	let infologfilename: std::path::PathBuf =
+		format_args!("logs/{}.info.log", logname).to_string().into();
+	let errorlogfilename: std::path::PathBuf =
+		format_args!("logs/{}.error.log", logname)
+			.to_string()
+			.into();
+
+	let sighup = Some(libc::SIGHUP);
 
 	fern::Dispatch::new()
 		.format(|out, message, record| {
@@ -20,17 +27,16 @@ pub fn start(logname: &str) -> Result<(), fern::InitError>
 				msg = message
 			))
 		})
-		.level(log::LevelFilter::Trace)
-		.chain(fern::log_file(tracelogfilename)?)
+		.chain(fern::log_reopen(&tracelogfilename, sighup)?)
 		.chain(
 			fern::Dispatch::new()
 				.level(log::LevelFilter::Debug)
-				.chain(fern::log_file(infologfilename)?),
+				.chain(fern::log_reopen(&infologfilename, sighup)?),
 		)
 		.chain(
 			fern::Dispatch::new()
 				.level(log::LevelFilter::Warn)
-				.chain(fern::log_file(errorlogfilename)?),
+				.chain(fern::log_reopen(&errorlogfilename, sighup)?),
 		)
 		.apply()?;
 	Ok(())
