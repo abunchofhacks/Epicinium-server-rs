aaaa4ec9155da95203f286d48404eb3e71941201
Sander in 't Veld <sanderintveld@hotmail.com>
2021-08-23 12:34:05 +0200

    Added license headers to all source files.

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,4 +1,25 @@
-/* Server */
+/*
+ * Part of epicinium_server
+ * developed by A Bunch of Hacks.
+ *
+ * Copyright (c) 2018-2021 A Bunch of Hacks
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * [authors:]
+ * Sander in 't Veld (sander@abunchofhacks.coop)
+ */
 
 use epicinium::Settings;
 use epicinium::Version;
--- src/server/message.rs
+++ src/server/message.rs
@@ -1,4 +1,25 @@
-/* Message */
+/*
+ * Part of epicinium_server
+ * developed by A Bunch of Hacks.
+ *
+ * Copyright (c) 2018-2021 A Bunch of Hacks
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * [authors:]
+ * Sander in 't Veld (sander@abunchofhacks.coop)
+ */
 
 use crate::common::header::*;
 use crate::common::keycode::*;
@@ -450,7 +471,9 @@ pub struct TagMetadata
 	pub supporter: bool,
 }
 
-#[derive(PartialEq, Eq, Copy, Clone, Serialize, Deserialize, Default, Debug)]
+#[derive(
+	PartialEq, Eq, Copy, Clone, Serialize, Deserialize, Default, Debug,
+)]
 pub struct LobbyMetadata
 {
 	#[serde(default, skip_serializing_if = "is_zero")]
